08961ffedae58b45b1011f33aee4b1ad5d9c6397
WMW <wnameless@gmail.com>
2014-03-08 13:14:19 -0500

    Add everyNewDieGetsDifferentInitialValue test

--- src/test/java/com/wmw/dice/CommonDieTest.java
+++ src/test/java/com/wmw/dice/CommonDieTest.java
@@ -1,12 +1,37 @@
 package com.wmw.dice;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CommonDieTest {
 
-  public static void main(String[] args) {
-    CommonDie die = new CommonDie();
-    if (!(die instanceof Die)) {
-      throw new RuntimeException("Die interface is not implemented.");
+  CommonDie die;
+
+  @Before
+  public void setUp() {
+    die = new CommonDie();
+  }
+
+  @Test
+  public void dieInterfaceMustBeImplemented() {
+    assertTrue(die instanceof Die);
+  }
+
+  @Test
+  public void everyNewDieGetsDifferentInitialValue() {
+    Set<Integer> values = new HashSet<Integer>();
+    for (int i = 0; i < 1000; i++) {
+      die = new CommonDie();
+      values.add(die.getValue());
     }
+    assertEquals(new HashSet<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6)), values);
   }
 
 }
